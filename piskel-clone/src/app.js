bf0fef81f85dca27f290fd0570ba1ea9740f367d
Dzmitry Ivanou <3prn@mail.ru>
2019-06-09 11:55:45 +0300

    refactor: add eslint, babel, webpack config

---
+++ piskel-clone/src/app.js
@@ -0,0 +1,7 @@
+import './screens/canvas/index.scss';
+import paintBacketIcon from './screens/canvas/assets/icons/paint_backet.png';
+
+const paintBacket = document.getElementById('paint-backet');
+paintBacket.src = paintBacketIcon;
+
+window.console.log('hello');
