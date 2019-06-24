b9aff72ae5e31559585ca07712e57f0f067ab417
Dzmitry Ivanou <3prn@mail.ru>
2019-06-24 15:11:46 +0300

    feat: add button move frame

--- piskel-clone/src/components/frame-list/FrameList.js
+++ piskel-clone/src/components/frame-list/FrameList.js
@@ -1,26 +1,28 @@
-import './index.scss';
+import './style.scss';
+
 import backgroundCanvasImg from '../../assets/images/canvas-backgrounds/canvas-background-light.png';
 
 export default class FrameList {
   constructor() {
     this.form = document;
+    this.dragSrcEl = null;
   }
 
   render() {
     const containerFrame = document.querySelector('.frame-container');
     const containerCurrentFrame = document.createElement('div');
-    containerCurrentFrame.className = 'container-current-frame';
+    containerCurrentFrame.className = 'container-current-frame draggable';
     const canvasFr = document.createElement('canvas');
     const buttonCanvasFrameDel = document.createElement('button');
-    buttonCanvasFrameDel.className = 'button-frame button-canvas-frame-del';
+    buttonCanvasFrameDel.className = 'button-frame button-del-frame';
     const buttonCloneFrame = document.createElement('button');
     buttonCloneFrame.className = 'button-frame button-clone-frame';
     const buttonCurrentNumber = document.createElement('input');
-    buttonCurrentNumber.className = 'button-frame button-current-number';
-    buttonCurrentNumber.value = document.querySelectorAll('.button-current-number').length + 1;
+    buttonCurrentNumber.className = 'button-frame button-current-number-frame';
+    buttonCurrentNumber.value = document.querySelectorAll('.button-current-number-frame').length + 1;
     buttonCurrentNumber.disabled = true;
     const buttonMove = document.createElement('button');
-    buttonMove.className = 'button-frame button-move';
+    buttonMove.className = 'button-frame button-move-frame';
     canvasFr.className = 'canvas-frame';
     canvasFr.width = '150';
     canvasFr.height = '150';
@@ -37,19 +39,29 @@ export default class FrameList {
     containerCurrentFrame.appendChild(buttonMove);
     containerCurrentFrame.appendChild(canvasFr);
     containerFrame.insertBefore(containerCurrentFrame, addButton);
-    containerFrame.addEventListener('click', this.click.bind(this));
+    containerFrame.addEventListener('mousedown', this.mouseDown.bind(this));
+    // buttonCanvasFrameDel.addEventListener('mousedown', this.mouseDown.bind(this));
+
+    const cols = document.querySelectorAll('.draggable');
+    [].forEach.call(cols, (col) => {
+      col.addEventListener('dragstart', this.handleDragStart.bind(this));
+      col.addEventListener('dragenter', this.handleDragEnter.bind(this));
+      col.addEventListener('dragover', this.handleDragOver.bind(this));
+      col.addEventListener('dragleave', this.handleDragLeave.bind(this));
+      col.addEventListener('drop', this.handleDrop.bind(this));
+      col.addEventListener('dragend', this.handleDragEnd.bind(this));
+    });
   }
 
-  click(event) {
+  mouseDown(event) {
     const currentNumberFrame = () => {
-      const frameCurrent = this.form.querySelectorAll('.button-current-number');
+      const frameCurrent = this.form.querySelectorAll('.button-current-number-frame');
       for (let i = 0; i < frameCurrent.length; i += 1) {
         frameCurrent[i].value = i + 1;
       }
     };
-    if (event.target.classList[1] === 'button-canvas-frame-del') {
+    if (event.target.classList[1] === 'button-del-frame') {
       event.target.parentNode.parentNode.removeChild(event.target.parentNode);
-      this.countFrame -= 1;
       currentNumberFrame();
     } else if (event.target.classList[1] === 'button-clone-frame') {
       const { nextSibling } = event.target.parentNode;
@@ -61,7 +73,56 @@ export default class FrameList {
       ctx.drawImage(cloneCanv, 0, 0, cloneCanv.width, cloneCanv.height, 0, 0, newCloneCanv.width, newCloneCanv.height);
       event.target.parentNode.parentNode.insertBefore(newCloneFrame, nextSibling);
       currentNumberFrame();
+
+      const cols = document.querySelectorAll('.container-current-frame');
+      [].forEach.call(cols, (col) => {
+        col.addEventListener('dragstart', this.handleDragStart.bind(this));
+        col.addEventListener('dragenter', this.handleDragEnter.bind(this));
+        col.addEventListener('dragover', this.handleDragOver.bind(this));
+        col.addEventListener('dragleave', this.handleDragLeave.bind(this));
+        col.addEventListener('drop', this.handleDrop.bind(this));
+        col.addEventListener('dragend', this.handleDragEnd.bind(this));
+      });
     }
     event.stopImmediatePropagation();
+  }
+
+  handleDragStart(event) {
+    this.dragSrcEl = event.target.parentNode;
+    // eslint-disable-next-line no-param-reassign
+    event.target.parentNode.style.opacity = 0.5;
+  }
+
+  handleDragEnter() {
+    this.dragSrcEl.classList.add('over');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  handleDragLeave() {
+    this.dragSrcEl.classList.remove('over');
+  }
+
+  handleDrop(event) {
+    const containerFrame = document.querySelector('.frame-container');
+    event.stopImmediatePropagation();
+    if (this.dragSrcEl !== event.target.parentNode) {
+      let curring = event.target.parentNode.nextElementSibling;
+      if (curring === this.dragSrcEl) curring = event.target.parentNode;
+      const ren = containerFrame.replaceChild(event.target.parentNode, this.dragSrcEl);
+      containerFrame.insertBefore(ren, curring);
+    }
+  }
+
+  handleDragEnd(event) {
+    // eslint-disable-next-line no-param-reassign
+    event.target.parentNode.style.opacity = '';
+    const cols = this.form.querySelectorAll('.draggable');
+    [].forEach.call(cols, (col) => {
+      col.classList.remove('over');
+    });
   }
 }
