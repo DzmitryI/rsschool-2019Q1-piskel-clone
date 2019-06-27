ba640473f48eedd43ef824158550a9b939f761ae
Dzmitry Ivanou <3prn@mail.ru>
2019-06-27 22:25:30 +0300

    feat: add cursor coordinates tool

--- piskel-clone/src/components/model-dialog/Resize/Resize.js
+++ piskel-clone/src/components/model-dialog/Resize/Resize.js
@@ -9,11 +9,18 @@ export default class Resize {
   init() {
     const resizeButton = this.form.querySelector('.resize-button');
     const resizeArr = this.form.getElementsByName('resize');
+    const cursorCoordinates = this.form.querySelector('.cursor-coordinates');
+    const currentResize = +[].filter.call(resizeArr, item => item.checked)[0].value;
+    cursorCoordinates.innerHTML = `[ ${currentResize}x${currentResize} ]`;
     this.currentSize = +[].filter.call(resizeArr, item => item.checked)[0].value;
     resizeButton.addEventListener('mousedown', this.mouseDown.bind(this));
   }
 
   mouseDown() {
+    const cursorCoordinates = this.form.querySelector('.cursor-coordinates');
+    const resizeArr = this.form.getElementsByName('resize');
+    const currentResize = +[].filter.call(resizeArr, item => item.checked)[0].value;
+    cursorCoordinates.innerHTML = `[ ${currentResize}x${currentResize} ]`;
     const canvas = this.form.querySelector('.canvas-conteiner__canvas');
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
--- piskel-clone/src/screens/canvas/index.js
+++ piskel-clone/src/screens/canvas/index.js
@@ -88,6 +88,8 @@ export default class Index {
     this.canvasData64 = canvasData64;
     this.canvasData128 = canvasData128;
     canvas.addEventListener('mousedown', this.mouseDown.bind(this));
+    canvas.addEventListener('mousemove', this.mouseCoordinates.bind(this));
+    canvas.addEventListener('mouseout', this.mouseCoordinatesOut.bind(this));
   }
 
   mouseDown(event) {
@@ -302,7 +304,7 @@ export default class Index {
     else if (event.which === 3) ctx.fillStyle = secondaryColor;
     const x = event.offsetX;
     const y = event.offsetY;
-    window.console.log(x, y);
+    // window.console.log(x, y);
     for (let i = 0; i < canvasData.length; i += 1) {
       const xi = canvasData[i][0] + canvasData[i][2];
       const yi = canvasData[i][1] + canvasData[i][3];
@@ -312,5 +314,33 @@ export default class Index {
         break;
       }
     }
+  }
+
+  mouseCoordinates(event) {
+    const resizeArr = this.form.getElementsByName('resize');
+    const cursorCoordinates = this.form.querySelector('.cursor-coordinates');
+    const currentResize = +[].filter.call(resizeArr, item => item.checked)[0].value;
+    let canvasData = [];
+    if (currentResize === 32) canvasData = this.canvasData32;
+    else if (currentResize === 64) canvasData = this.canvasData64;
+    else if (currentResize === 128) canvasData = this.canvasData128;
+    const x = event.offsetX;
+    const y = event.offsetY;
+    for (let i = 0; i < canvasData.length; i += 1) {
+      const xi = canvasData[i][0] + canvasData[i][2];
+      const yi = canvasData[i][1] + canvasData[i][3];
+      if ((x <= xi) && (y <= yi)) {
+        // eslint-disable-next-line max-len
+        cursorCoordinates.innerHTML = `[ ${currentResize}x${currentResize} ] ${canvasData[i][4]} : ${canvasData[i][5]}`;
+        break;
+      }
+    }
+  }
+
+  mouseCoordinatesOut() {
+    const resizeArr = this.form.getElementsByName('resize');
+    const cursorCoordinates = this.form.querySelector('.cursor-coordinates');
+    const currentResize = +[].filter.call(resizeArr, item => item.checked)[0].value;
+    cursorCoordinates.innerHTML = `[ ${currentResize}x${currentResize} ]`;
   }
 }
